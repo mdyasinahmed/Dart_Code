483f4def04d72edfeb116111be7e8816f8065758
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-14 16:16:12 +0600

    dart variable code added

--- projects/hello.dart
+++ projects/hello.dart
@@ -1,3 +1,10 @@
 void main() {
+  // Dart is Cool
   print("Hello World!");
+
+  /**
+   * multiline comment in Dart
+   */
+
+  // documentation type comment
 }
---
+++ projects/variables.dart
@@ -0,0 +1,29 @@
+void main() {
+  int a = 5;
+  int b = 6;
+  print(a + b);
+
+  double pi = 3.14157;
+  double r = 5;
+  double area = 2 * pi * r;
+  print(area);
+
+  String str = "hello world";
+  print(str);
+
+  bool s1 = true;
+  bool s2 = false;
+  print(s1);
+
+  dynamic dyn = "hello dynamic";
+  print("dynamic 1 - $dyn");
+
+  dyn = 3.23324;
+  print("dynamic 2 - $dyn");
+
+  var v1 = "hello variable";
+  print(v1);
+
+  final strg = "hello final";
+  print(strg);
+}
