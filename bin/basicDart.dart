632ef009d309e11d75ad7ec43deaf86f4304e2f0
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-30 08:40:04 +0600

    add dart practice code

---
+++ bin/basicDart.dart
@@ -0,0 +1,20 @@
+void main(){
+  print("Hello World!");
+  var x = 10;
+  var y = 20;
+  var z = x+y;
+  print(z);
+
+  print("DART DataTypes - ");
+  var intNum = 10;
+  var doubleNum = 10.10;
+  print(intNum+doubleNum);
+
+  print("Dart String");
+  var str = "Practicing Dart to learn flutter";
+  print(str);
+  var str2 = "Flutter is nothing but a framework of dart.";
+  print(str2);
+  
+
+}
