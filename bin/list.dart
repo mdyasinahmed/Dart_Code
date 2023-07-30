e320e51a118862af1182638f9602814f21d18872
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-30 08:55:36 +0600

    add dart list practice code

---
+++ bin/list.dart
@@ -0,0 +1,19 @@
+void main(){
+  //dart list
+  var numbers = [81, 90, 95, 100];
+  print(numbers);
+  print(numbers[0]);
+  print(numbers[2]);
+
+  var cities = ['Sylhet', 'Comilla', 'Chittagong', 'Dhaka', 'Barisal'];
+  print(cities);
+  print(cities[0]);
+  print(cities[1]);
+
+  var collections = [10, 10.10, "flutter", true];
+  print(collections);
+  print(collections[0]);
+  print(collections[1]);
+  print(collections[2]);
+  print(collections[3]);
+}
