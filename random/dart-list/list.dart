6e9ae203713830e43321ec0fa978d17620a67c92
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-02 23:41:41 +0600

    update dart list code

--- random/dart-list/list.dart
+++ random/dart-list/list.dart
@@ -1,7 +1,15 @@
 void main() {
+  // Dart list practice
   var list = [1, 2 , 3];
-
   print(list);
 
+  var vehicleList = ['car', 'bus', 'boat', 'plane'];
+  print(vehicleList);
+
+  var multipleElement = ['yasin', 20, 95.50, true];
+  print(multipleElement);
+
+  var myCity = ['Dhaka', 'Sylhet', 'Khulna', 'barisal'];
+  print(myCity);
 
 }
