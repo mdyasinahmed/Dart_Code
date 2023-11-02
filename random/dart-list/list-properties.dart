1d8f2f7312570e5327ef80e0011d89338dd03b88
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-02 23:53:32 +0600

    add dart list properties code

---
+++ random/dart-list/list-properties.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  var myCity = ['Dhaka', 'Sylhet', 'Khulna', 'barisal'];
+
+  print("first element = " + myCity.first);
+
+  int getLen = myCity.length;
+  print(getLen);
+
+  print(myCity.reversed);
+
+  print(myCity.isEmpty);
+  print(myCity.last);
+  print(myCity.isNotEmpty);
+  
+
+}
