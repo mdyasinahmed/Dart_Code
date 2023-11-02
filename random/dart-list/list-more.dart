02388d8d03dbb9e7763f0caa0018609a97582c0e
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-02 23:57:51 +0600

    add growable length list code

--- random/dart-list/list-more.dart
+++ random/dart-list/list-more.dart
@@ -2,4 +2,10 @@ void main() {
   // fixed length list
   const myList = ['Sylhet', 'Dhaka', 'Khulna'];
   print(myList);
+
+  // growable length list
+  var myList2 =[1, 3, 5, 7];
+  print(myList2);
+  myList2.add(9);
+  print(myList2);
 }
