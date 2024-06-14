d71c8e621bf50c47f4923b98eb340293601c2f9a
mdyasinahmed <yasinahmed7875@gmail.com>
2024-06-14 21:36:14 +0600

    update dart practice code

--- random/dart-list/list.dart
+++ random/dart-list/list.dart
@@ -3,10 +3,10 @@ void main() {
   var list = [1, 2 , 3];
   print(list);
 
-  var vehicleList = ['car', 'bus', 'boat', 'plane'];
+  var vehicleList = ['car', 'bus', 'boat', 'plane', 'train'];
   print(vehicleList);
 
-  var multipleElement = ['yasin', 20, 95.50, true];
+  var multipleElement = ['yasin', 21, 95.50, true];
   print(multipleElement);
 
   var myCity = ['Dhaka', 'Sylhet', 'Khulna', 'barisal'];
